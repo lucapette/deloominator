90a73dd6d16bc640fa57a3c45c70f369de3deeff
lucapette <lucapette@gmail.com>
2016-12-25 23:15:42 +0100

    Simplify API for db

--- db/db.go
+++ db/db.go
@@ -1,23 +1,71 @@
 package db
 
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
 type DataSource struct {
-	Name string
+	Driver   string
+	Username string
+	Pass     string
+	Host     string
+	Port     int
+	DBName   string
+	Options  string
 }
 
+// support for https://en.wikipedia.org/wiki/Data_source_name
+var validDSN = regexp.MustCompile(`(?P<driver>[^:]+)://(?P<cred>(?P<username>[^:]+):(?P<pass>[^@]+)@)?(?P<host>[^:]+)(?P<opt_port>:(?P<port>[^/]+))?/(?P<db_name>[^?]+)\??(?P<options>.+)?`)
+
 type DataSources map[string]*DataSource
+
+func NewDataSource(source string) (ds *DataSource, err error) {
+	fields := validDSN.FindStringSubmatch(source)
+
+	if fields == nil {
+		return ds, fmt.Errorf("%s is not a valid Data Source Name", source)
+	}
+
+	groupedMatches := make(map[string]string, len(fields))
+	for i, match := range fields {
+		groupedMatches[validDSN.SubexpNames()[i]] = match
+	}
+
+	var port int
+
+	if len(groupedMatches["port"]) > 0 {
+		port, err = strconv.Atoi(groupedMatches["port"])
+
+		if err != nil {
+			return ds, fmt.Errorf("%s is not a valid Data Source Name. %s is not a valid port", source, groupedMatches["port"])
+		}
+	}
+
+	ds = &DataSource{
+		Driver:   groupedMatches["driver"],
+		Username: groupedMatches["username"],
+		Pass:     groupedMatches["pass"],
+		Host:     groupedMatches["host"],
+		Port:     port,
+		DBName:   groupedMatches["db_name"],
+		Options:  groupedMatches["options"],
+	}
+
+	return ds, nil
+}
 
 func NewSources(dataSources []string) (sources DataSources, err error) {
 	sources = make(DataSources, len(dataSources))
 
 	for _, source := range dataSources {
-		dsn, err := ParseDSN(source)
+		ds, err := NewDataSource(source)
 		if err != nil {
 			return nil, err
 		}
 
-		sources[dsn.DBName] = &DataSource{
-			Name: dsn.DBName,
-		}
+		sources[ds.DBName] = ds
 	}
 
 	return sources, nil
--- db/db_test.go
+++ db/db_test.go
@@ -1,10 +1,64 @@
 package db_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/lucapette/deluminator/db"
 )
+
+func TestNewSource(t *testing.T) {
+	sourceTests := []struct {
+		source   string
+		expected *db.DataSource
+	}{
+		{ // full
+			"postgresql://grace:hopper@hal9000:3000/test?foo=bar",
+			&db.DataSource{
+				Driver:   "postgresql",
+				Username: "grace",
+				Pass:     "hopper",
+				Host:     "hal9000",
+				Port:     3000,
+				DBName:   "test",
+				Options:  "foo=bar",
+			},
+		},
+		{ // no port
+			"postgresql://grace:hopper@hal9000/test?foo=bar",
+			&db.DataSource{
+				Driver:   "postgresql",
+				Username: "grace",
+				Pass:     "hopper",
+				Host:     "hal9000",
+				DBName:   "test",
+				Options:  "foo=bar",
+			},
+		},
+		{ // no credentials
+			"postgresql://hal9000:3000/test?foo=bar",
+			&db.DataSource{
+				Driver:  "postgresql",
+				Host:    "hal9000",
+				Port:    3000,
+				DBName:  "test",
+				Options: "foo=bar",
+			},
+		},
+	}
+
+	for _, test := range sourceTests {
+		actual, err := db.NewDataSource(test.source)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !reflect.DeepEqual(actual, test.expected) {
+			t.Fatalf("Expected %v to equal %v", actual, test.expected)
+		}
+	}
+}
 
 func TestNewSources(t *testing.T) {
 	dataSources := []string{"postgresql://localhost/test"}
