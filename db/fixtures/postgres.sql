1a8adcc6e325f235eeb71ca2a770702400ad198a
lucapette <lucapette@gmail.com>
2016-12-30 19:17:17 +0100

    Less is more

--- db/fixtures/postgres.sql
+++ db/fixtures/postgres.sql
@@ -1,10 +1,3 @@
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
 DROP DATABASE IF EXISTS {{.Name}};
 CREATE DATABASE {{.Name}};
 
