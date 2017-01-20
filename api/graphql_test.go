1afe7be4543612448bac9d15c1c6d11ff449e13b
lucapette <lucapette@gmail.com>
2017-01-20 15:32:02 +0100

    Embrace the pattern

--- api/graphql_test.go
+++ api/graphql_test.go
@@ -2,7 +2,6 @@ package api_test
 
 import (
 	"bytes"
-	"encoding/json"
 	"flag"
 	"io/ioutil"
 	"net/http/httptest"
@@ -10,7 +9,6 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/graphql-go/graphql"
 	"github.com/lucapette/deluminator/api"
 	"github.com/lucapette/deluminator/db"
 	"github.com/lucapette/deluminator/testutil"
@@ -18,7 +16,17 @@ import (
 
 var update = flag.Bool("update", false, "update golden files")
 
-func TestDataSources(t *testing.T) {
+func TestGraphQLQueries(t *testing.T) {
+	tests := []struct {
+		query   string
+		code    int
+		fixture string
+	}{
+		{query: "{notAQuery}", code: 400, fixture: "wrong_query.json"},
+		{query: "{DataSources {name}}", code: 200, fixture: "data_sources.json"},
+		{query: "{DataSources {name tables {name}}}", code: 200, fixture: "data_sources_with_tables.json"},
+	}
+
 	dsn, cleanup := testutil.SetupDB(db.PostgresDriver, t)
 	app := testutil.InitApp(t, map[string]string{
 		"DATA_SOURCES": dsn.Format(),
@@ -29,99 +37,33 @@ func TestDataSources(t *testing.T) {
 		cleanup()
 	}()
 
-	req := httptest.NewRequest("POST", "http://example.com/graphql", strings.NewReader("{DataSources {name}}"))
+	for _, test := range tests {
+		t.Run(test.fixture, func(t *testing.T) {
+			req := httptest.NewRequest("POST", "http://example.com/graphql", strings.NewReader(test.query))
 
-	w := httptest.NewRecorder()
+			w := httptest.NewRecorder()
 
-	api.GraphQLHandler(app)(w, req)
+			api.GraphQLHandler(app)(w, req)
 
-	resp, err := ioutil.ReadAll(w.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	actual := string(resp)
+			resp, err := ioutil.ReadAll(w.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			actual := string(resp)
 
-	if w.Code != 200 {
-		t.Fatalf("expected code 200, got: %v. Resp: %v", w.Code, actual)
-	}
+			if w.Code != test.code {
+				t.Fatalf("expected code %d, got: %d. Resp: %s", test.code, w.Code, actual)
+			}
 
-	var expected bytes.Buffer
-	testutil.ParseFixture(t, &expected, "data_sources.json", testutil.DBTemplate{Name: dsn.DBName})
-	if *update {
-		testutil.WriteFixture(t, "data_sources.json", actual)
-	}
+			var expected bytes.Buffer
+			testutil.ParseFixture(t, &expected, test.fixture, testutil.DBTemplate{Name: dsn.DBName})
+			if *update {
+				testutil.WriteFixture(t, test.fixture, actual)
+			}
 
-	if !reflect.DeepEqual(expected.String(), actual) {
-		t.Fatalf("Unexpected result, diff: %v", testutil.Diff(expected.String(), actual))
-	}
-}
-
-func TestDataSourcesWithTables(t *testing.T) {
-	dsn, cleanup := testutil.SetupDB(db.PostgresDriver, t)
-	app := testutil.InitApp(t, map[string]string{
-		"DATA_SOURCES": dsn.Format(),
-	})
-
-	defer func() {
-		app.Shutdown()
-		cleanup()
-	}()
-
-	req := httptest.NewRequest("POST", "http://example.com/graphql", strings.NewReader("{DataSources {name tables {name}}}"))
-
-	w := httptest.NewRecorder()
-
-	api.GraphQLHandler(app)(w, req)
-
-	resp, err := ioutil.ReadAll(w.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	actual := string(resp)
-
-	if w.Code != 200 {
-		t.Fatalf("expected code 200, got: %v. Resp: %v", w.Code, actual)
-	}
-
-	var expected bytes.Buffer
-	testutil.ParseFixture(t, &expected, "data_sources_with_tables.json", testutil.DBTemplate{Name: dsn.DBName})
-	if *update {
-		testutil.WriteFixture(t, "data_sources_with_tables.json", actual)
-	}
-
-	if !reflect.DeepEqual(expected.String(), actual) {
-		t.Fatalf("Unexpected result, diff: %v", testutil.Diff(expected.String(), actual))
-	}
-}
-
-func TestGraphQLPOSTWrongQuery(t *testing.T) {
-	app := testutil.InitApp(t, map[string]string{}) // ugly signature. Needs fixing.
-
-	defer func() {
-		app.Shutdown()
-	}()
-
-	req := httptest.NewRequest("POST", "http://example.com/graphql", strings.NewReader("{notAQuery}"))
-	w := httptest.NewRecorder()
-	api.GraphQLHandler(app)(w, req)
-
-	if w.Code != 400 {
-		t.Fatalf("expected code 400, got: %v", w.Code)
-	}
-
-	resp, err := ioutil.ReadAll(w.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	var actual *graphql.Result
-
-	err = json.Unmarshal(resp, &actual)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if !actual.HasErrors() {
-		t.Fatal("Expected error but got none")
+			if !reflect.DeepEqual(expected.String(), actual) {
+				t.Fatalf("Unexpected result, diff: %v", testutil.Diff(expected.String(), actual))
+			}
+		})
 	}
 }
