dd69f4aa3b7c18c908554635747271f9d930bb71
lucapette <lucapette@gmail.com>
2017-04-07 21:31:23 +0200

    Add selected dataSource

--- ui/src/PlaygroundPage.js
+++ ui/src/PlaygroundPage.js
@@ -7,8 +7,23 @@ import { Button, Container, Form, Grid } from 'semantic-ui-react'
 import { gql, graphql } from 'react-apollo';
 
 class Playground extends Component {
+  state: {
+    selectedDataSource: string;
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      selectedDataSource: ''
+    };
+  }
+
   dataSourcesOptions = (data) => {
-    return data.dataSources ? data.dataSources.map((s) => ({text: s.name, value: s.name})) : [];
+    return data.dataSources ? data.dataSources.map((s) => ({name: s.name, text: s.name, value: s.name})) : [];
+  }
+
+  handleDataSourcesChange = (e, { value }) => {
+    this.setState({selectedDataSource: value});
   }
 
   render() {
@@ -19,8 +34,12 @@ class Playground extends Component {
             <Grid.Column>
               <Form>
                 <Form.Group>
-                  <Form.Dropdown placeholder='Data Source' search selection options={this.dataSourcesOptions(this.props.data)} />
-                  <Button icon='play' primary content='Run'/>
+                  <Form.Dropdown
+                    placeholder='Data Source'
+                    search selection
+                    onChange={this.handleDataSourcesChange}
+                    options={this.dataSourcesOptions(this.props.data)} />
+                  <Button icon='play' primary content='Run' disabled={!this.state.selectedDataSource}/>
                 </Form.Group>
                 <Form.TextArea placeholder='Write your query here' />
               </Form>
