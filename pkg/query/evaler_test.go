4940240d23fefa2d9a6468764625885af7fcc775
lucapette <lucapette@gmail.com>
2017-12-27 13:10:21 +0100

    Add more tests for evaler

--- pkg/query/evaler_test.go
+++ pkg/query/evaler_test.go
@@ -8,42 +8,47 @@ import (
 
 var variables = query.Variables{
 	"{timestamp}": "2017-10-13T23:59:59Z",
+	"{today}":     "2017-10-15",
+	"{yesterday}": "2015-04-15",
 }
 
 func TestEval(t *testing.T) {
-	type args struct {
-		query string
-	}
 	tests := []struct {
-		name string
-		args args
-		want string
+		name  string
+		query string
+		want  string
 	}{
 		{
 			"no variables",
-			args{
-				"select * from answers",
-			},
+			"select * from answers",
 			"select * from answers",
 		},
 		{
 			"{timestamp}",
-			args{
-				"select * from answers where created_at < {timestamp}",
-			},
+			"select * from answers where created_at < {timestamp}",
 			`select * from answers where created_at < '2017-10-13T23:59:59Z'`,
 		},
+		{
+			"{today}",
+			"select * from answers where created_at < {today}",
+			`select * from answers where created_at < '2017-10-15'`,
+		},
+		{
+			"{yesterday}",
+			"select * from answers where created_at < {yesterday}",
+			`select * from answers where created_at < '2015-04-15'`,
+		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
 			evaler := query.NewEvaler(variables)
-			got, err := evaler.Eval(tt.args.query)
+			got, err := evaler.Eval(test.query)
 			if err != nil {
 				t.Errorf("could not eval: %v", err)
 			}
 
-			if got != tt.want {
-				t.Errorf("Eval() = %v, want %v", got, tt.want)
+			if got != test.want {
+				t.Errorf("Eval() = %v, want %v", got, test.want)
 			}
 		})
 	}
