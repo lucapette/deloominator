e116a5475e932a86e00e595447ccd6de4d494803
lucapette <lucapette@gmail.com>
2018-01-05 18:49:36 +0100

    Show variables in question

--- ui/src/pages/Questions/Question.js
+++ ui/src/pages/Questions/Question.js
@@ -2,11 +2,36 @@ import React, {Component} from 'react';
 import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
 import DocumentTitle from 'react-document-title';
-import {Container, Message, Loader, Grid, Header} from 'semantic-ui-react';
+import {Container, Message, Loader, Grid, Header, Form} from 'semantic-ui-react';
 
 import QueryResult from '../../components/QueryResult';
+import QueryVariables from '../../components/QueryVariables';
 
 class QuestionContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      variables: [],
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {data: {loading, error, question}} = nextProps;
+
+    if (loading || error) {
+      return;
+    }
+
+    const {variables} = question;
+
+    this.setState({variables: variables.map(v => ({name: v.name, value: v.value, isControllable: v.isControllable}))});
+  }
+
+  handleVariableChange = (key, value) => {
+    const {variables} = this.state;
+    const index = variables.findIndex(e => e['name'] == key);
+    this.setState({variables: variables.map((item, i) => (index !== i ? item : {...item, value: value}))});
+  };
   render() {
     const {data: {loading, error, question}} = this.props;
 
@@ -32,11 +57,16 @@ class QuestionContainer extends Component {
           <Header as="h1">{title}</Header>
           <Grid.Row>
             <Grid.Column>
-              <QueryResult
-                source={dataSource}
-                query={query}
-                variables={variables.map(v => ({name: v.name, value: v.value, isControllable: v.isControllable}))}
-              />
+              <Form>
+                <Form.Group>
+                  <QueryVariables variables={this.state.variables} handleVariableChange={this.handleVariableChange} />
+                </Form.Group>
+              </Form>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <QueryResult source={dataSource} query={query} variables={this.state.variables} />
             </Grid.Column>
           </Grid.Row>
         </Container>
